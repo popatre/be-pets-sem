e191c4c17164ad7d962a89a4f3edf41fd5b51f5f
popatre <76402245+popatre@users.noreply.github.com>
2023-03-10 17:29:42 +0000

    add sem 2 solution

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { readFile } = require("fs/promises");
+const { readFile, readdir } = require("fs/promises");
 
 app.set("port", process.env.PORT || 3000);
 
@@ -16,6 +16,31 @@ app.get("/api/bands/:id", (req, res) => {
             res.status(200).send({ band: bandData });
         }
     );
+});
+
+app.get("/api/songs", (req, res) => {
+    const genre = req.query.genre;
+    readdir(`${__dirname}/data/songs`)
+        .then((fileNames) => {
+            const pendingReads = fileNames.map((fileName) => {
+                return readFile(`${__dirname}/data/songs/${fileName}`, "utf-8");
+            });
+            return Promise.all(pendingReads);
+        })
+        .then((fileContents) => {
+            const songs = fileContents.map((file) => {
+                return JSON.parse(file);
+            });
+
+            if (genre) {
+                const requestedSongs = songs.filter((song) => {
+                    return song.genre === genre;
+                });
+                res.status(200).send({ songs: requestedSongs });
+            } else {
+                res.status(200).send({ songs });
+            }
+        });
 });
 
 app.listen(app.get("port"), () => {
